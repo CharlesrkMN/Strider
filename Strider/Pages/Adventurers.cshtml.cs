6ad69db134e29ea3ba31fc35d1e2d5c5dcdd362b
Charles Kramer <112837649+CharlesrkMN@users.noreply.github.com>
2024-12-06 02:59:21 +0000

    Adventurers Page UI

---
+++ Strider/Pages/Adventurers.cshtml.cs
@@ -0,0 +1,80 @@
+   using Microsoft.AspNetCore.Mvc;
+   using Microsoft.AspNetCore.Mvc.RazorPages;
+   using Microsoft.AspNetCore.Mvc.Rendering;
+   using Microsoft.Data.Sqlite;
+   using System.Collections.Generic;
+
+   public class AdventurersModel : PageModel
+   {
+       public List<SelectListItem> AdventurerList { get; set; }
+       public Adventurer SelectedAdventurer { get; set; }
+
+       public void OnGet()
+       {
+           LoadAdventurerList();
+       }
+
+       public void OnPost(string selectedAdventurer)
+       {
+           LoadAdventurerList();
+           if (!string.IsNullOrEmpty(selectedAdventurer))
+           {
+               SelectedAdventurer = GetAdventurerByadv_ID(int.Parse(selectedAdventurer));
+           }
+       }
+
+       private void LoadAdventurerList()
+       {
+           AdventurerList = new List<SelectListItem>();
+           using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT adv_ID, adv_Type FROM Adventurers";
+               using (var reader = command.ExecuteReader())
+               {
+                   while (reader.Read())
+                   {
+                       AdventurerList.Add(new SelectListItem
+                       {
+                           Value = reader.GetInt32(0).ToString(),
+                           Text = reader.GetString(1)
+                       });
+                   }
+               }
+           }
+       }
+
+       private Adventurer GetAdventurerByadv_ID(int id)
+       {
+           using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT * FROM Adventurers WHERE adv_ID = @adv_ID";
+               command.Parameters.AddWithValue("@adv_ID", id);
+               using (var reader = command.ExecuteReader())
+               {
+                   if (reader.Read())
+                   {
+                       return new Adventurer
+                       {
+                           adv_ID = reader.GetInt32(0),
+                           adv_Type = reader.GetString(1),
+                           adv_Race = reader.GetString(2),
+                           ImageFileName = reader.GetString(3)
+                       };
+                   }
+               }
+           }
+           return null;
+       }
+   }
+
+   public class Adventurer
+   {
+       public int adv_ID { get; set; }
+       public string adv_Type { get; set; }
+       public string adv_Race { get; set; }
+       public string ImageFileName { get; set; }
+   }
---
+++ Strider/Pages/Monsters.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class MonstersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
