69f8ed7e8b26d34b579bd7d0a50afecff864e133
Charles Kramer <112837649+CharlesrkMN@users.noreply.github.com>
2024-12-11 21:54:36 +0000

    Begin working on combat log

--- Strider/Pages/Play.cshtml.cs
+++ Strider/Pages/Play.cshtml.cs
@@ -1,54 +1,127 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.Sqlite;
 using System.Collections.Generic;
-public class PlayModel : PageModel {
-    public Player Pchoice {get; set;}
-    //public Enemy
-    protected void mWarrior()
-    {
-        //hero.src = "/aImages/@Model.Pchoice.ImageFileName";
-    }
-    public async Task OnPostmWarrior()
-    {
-            Pchoice = GetPlayerByadv_ID(1);
-    }
+using System.Security.Cryptography;
 
-    public async Task OnPostfWarrior()
-    {
-            Pchoice = GetPlayerByadv_ID(2);
-    }
+public class PlayModel : PageModel
+   {
+       public List<SelectListItem> PlayerList { get; set; }
+       public List<SelectListItem> EnemyList { get; set; }
+       public Player SelectedPlayer { get; set; }
+       public Enemy SelectedEnemy {get; set;}
+       public pAbility SelectedAbility {get; set;}
+       public pAbility SelectedAbility2 {get; set;}
+       public pStat SelectedpStat {get; set;}
+       public mStat SelectedmStat {get; set;}
 
-    public async Task OnPostmArcher()
-    {
-            Pchoice = GetPlayerByadv_ID(3);
-    }
+       public void Versus()
+       {
+            Random rnd = new Random();
+            int x = rnd.Next(1,8);
+            SelectedEnemy = GetEnemyByMon_ID(x);
+       }
 
-    public async Task OnPostfArcher()
-    {
-            Pchoice = GetPlayerByadv_ID(4);
-    }
+        public void LoadEnemyList()
+       {
+           EnemyList = new List<SelectListItem>();
+           using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT Mon_ID, Mon_Type FROM Monsters";
+               using (var reader = command.ExecuteReader())
+               {
+                   while (reader.Read())
+                   {
+                       EnemyList.Add(new SelectListItem
+                       {
+                           Value = reader.GetInt32(0).ToString(),
+                           Text = reader.GetString(1)
+                       });
+                   }
+               }
+           }
+       }
 
-    public async Task OnPostmMage()
-    {
-            Pchoice = GetPlayerByadv_ID(5);
-    }
+    public Enemy GetEnemyByMon_ID(int id)
+       {
+           using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT * FROM Monsters WHERE Mon_ID = @Mon_ID";
+               command.Parameters.AddWithValue("@Mon_ID", id);
+               using (var reader = command.ExecuteReader())
+               {
+                   if (reader.Read())
+                   {
+                       return new Enemy
+                       {
+                           Mon_ID = reader.GetInt32(0),
+                           Mon_Type = reader.GetString(1),
+                           Mon_Description = reader.GetString(2),
+                           ImageFileName = reader.GetString(3)
+                       };
+                   }
+               }
+           }
+           return null;
+       }
+   
 
-    public async Task OnPostfMage()
-    {
-            Pchoice = GetPlayerByadv_ID(6);
-    }
-    public Player GetPlayerByadv_ID(int id)
-    {
-        using (var connection = new SqliteConnection("Data Source=Strider.db"))
-        {
-            connection.Open();
-            var command = connection.CreateCommand();
-            command.CommandText = "SELECT * FROM Adventurers WHERE adv_ID = id";
-            using (var reader = command.ExecuteReader())
-            {
-                if (reader.Read())
+       public void OnGet()
+       {
+           LoadPlayerList();
+       }
+
+       public void OnPost(string selectedPlayer)
+       {
+           LoadPlayerList();
+           if (!string.IsNullOrEmpty(selectedPlayer))
+           {
+               SelectedPlayer = GetPlayerByadv_ID(int.Parse(selectedPlayer));
+               SelectedAbility = GetAbilityByadv_ID(int.Parse(selectedPlayer));
+               SelectedAbility2 = GetAbility2Byadv_ID(int.Parse(selectedPlayer));
+           }
+           Versus();
+       }
+
+       public void LoadPlayerList()
+       {
+           PlayerList = new List<SelectListItem>();
+           using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT adv_ID, adv_Type FROM Adventurers";
+               using (var reader = command.ExecuteReader())
+               {
+                   while (reader.Read())
+                   {
+                       PlayerList.Add(new SelectListItem
+                       {
+                           Value = reader.GetInt32(0).ToString(),
+                           Text = reader.GetString(1)
+                       });
+                   }
+               }
+           }
+       }
+
+       public Player GetPlayerByadv_ID(int id)
+       {
+           using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT * FROM Adventurers WHERE adv_ID = @adv_ID";
+               command.Parameters.AddWithValue("@adv_ID", id);
+               using (var reader = command.ExecuteReader())
+               {
+                   if (reader.Read())
                    {
                        return new Player
                        {
@@ -62,7 +135,62 @@ public class PlayModel : PageModel {
            }
            return null;
        }
-    }
+       public pAbility GetAbilityByadv_ID(int id)
+       {
+            using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT a.* FROM Abilities AS a INNER JOIN Adventurers as t ON a.adv_ID = t.adv_ID WHERE t.adv_ID = @adv_ID";
+               command.Parameters.AddWithValue("@adv_ID", id);
+               using (var reader = command.ExecuteReader())
+               {
+                   if (reader.Read())
+                   {
+                       return new pAbility
+                       {
+                           Ability_ID = reader.GetInt32(0),
+                           Action = reader.GetString(1),
+                           Damage = reader.GetInt32(2),
+                           adv_ID = reader.GetInt32(3),
+                           Mon_ID = reader.GetInt32(4)
+                       };
+                   }
+               }
+           }
+           return null;
+       }
+
+        public pAbility GetAbility2Byadv_ID(int id)
+       {
+            using (var connection = new SqliteConnection("Data Source=Strider.db"))
+           {
+               connection.Open();
+               var command = connection.CreateCommand();
+               command.CommandText = "SELECT a.* FROM Abilities AS a INNER JOIN Adventurers as t ON a.adv_ID = t.adv_ID WHERE t.adv_ID = @adv_ID ORDER BY a.Ability_ID DESC";
+               command.Parameters.AddWithValue("@adv_ID", id);
+               using (var reader = command.ExecuteReader())
+               {
+                   if (reader.Read())
+                   {
+                       return new pAbility
+                       {
+                           Ability_ID = reader.GetInt32(0),
+                           Action = reader.GetString(1),
+                           Damage = reader.GetInt32(2),
+                           adv_ID = reader.GetInt32(3),
+                           Mon_ID = reader.GetInt32(4)
+                       };
+                   }
+               }
+           }
+           return null;
+       }
+       public void OnAttack()
+       {
+
+       }
+   }
 
    public class Player
    {
@@ -71,3 +199,50 @@ public class PlayModel : PageModel {
        public string adv_Race { get; set; }
        public string ImageFileName { get; set; }
    }
+
+   public class Enemy
+   {
+        public int Mon_ID { get; set; }
+       public string Mon_Type { get; set; }
+       public string Mon_Description { get; set; }
+       public string ImageFileName { get; set; }
+   }
+
+   public class pAbility
+   {
+        public int Ability_ID {get; set;}
+        public string Action {get; set;}
+        public int Damage {get; set;}
+        public int adv_ID {get;set;}
+        public int Mon_ID {get; set;}
+   }
+
+      public class mAbility
+   {
+        public int Ability_ID {get; set;}
+        public string Action {get; set;}
+        public int Damage {get; set;}
+        public int adv_ID {get;set;}
+        public int Mon_ID {get; set;}
+   }
+
+   public class pStat
+   {
+        public int Stat_ID {get; set;}
+        public int Health {get; set;}
+        public int Speed {get; set;}
+        public int Exp {get; set;}
+        public int adv_ID {get; set;}
+        public int Mon_ID {get; set;}
+   }
+
+   public class mStat
+   {
+        public int Stat_ID {get; set;}
+        public int Health {get; set;}
+        public int Speed {get; set;}
+        public int Exp {get; set;}
+        public int adv_ID {get; set;}
+        public int Mon_ID {get; set;}
+   }
+
--- Strider/obj/Debug/net8.0/Strider.AssemblyInfo.cs
+++ Strider/obj/Debug/net8.0/Strider.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strider")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2102756c1fd042ffe21fa4774cc5d68bbc998acf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6280a62ce85cec21cefbbaae14272aa975a851")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strider")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strider")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
